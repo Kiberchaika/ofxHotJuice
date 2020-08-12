652f7429628d935ab909be402fc29bfede102426
andorxornot <andorxornot@gmail.com>
2020-08-12 20:05:58 +0300

    Fixed python script

---
+++ example_oF_subplugin/project_settings.py
@@ -0,0 +1,6 @@
+prj_file = 'example_oF_subplugin'
+path_hotjuice_win = 'D:/Developer/Kiberchaika/hotjuice/src/'
+path_resources = 'D:/Developer/Crossplatform/of_v0.10.1_vs2017_release/addons/ofxHotJuice/resources'
+company_name = "MyCompany"
+bundle_id = 'com.company.application'
+
--- example_oF_subplugin/project_setup_win.py
+++ example_oF_subplugin/project_setup_win.py
@@ -1,11 +1,6 @@
 from lxml import etree
 import os
-
-prj_file = 'example_oF_subplugin'
-path_hotjuice_win = 'D:/Developer/Kiberchaika/hotjuice/src/'
-path_resources = 'D:/Developer/Crossplatform/of_v0.10.1_vs2017_release/addons/ofxHotJuice/resources'
-company_name = "MyCompany"
-bundle_id = 'com.company.application'
+from project_settings import *
 
 tree = etree.parse(prj_file + '.vcxproj')
 
