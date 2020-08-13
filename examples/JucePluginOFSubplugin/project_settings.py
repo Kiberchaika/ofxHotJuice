01620736fa20c0976df4ed03932396125bb292fc
andorxornot <andorxornot@gmail.com>
2020-08-13 21:14:37 +0300

    Updated project settings

--- examples/JucePluginOFSubplugin/project_settings.py
+++ examples/JucePluginOFSubplugin/project_settings.py
@@ -1,7 +1,3 @@
-project_name = 'example_oF_subplugin'
-path_hotjuice_win = 'D:/Developer/Kiberchaika/hotjuice/src/'
-path_hotjuice_osx = ''
-path_resources_win = 'D:/Developer/Crossplatform/of_v0.10.1_vs2017_release/addons/ofxHotJuice/examples/JucePluginOFSubplugin/resources'
-path_resources_osx = ''
-company_name = "MyCompany"
+project_name = 'Hotjuice oF and JUCE example'
 bundle_id = 'com.company.application'
+company_name = "MyCompany"
